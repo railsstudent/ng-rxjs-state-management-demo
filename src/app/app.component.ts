31aa466bf921ed4265f31dbda1b01bec1a0f9a09
Connie Leung <cleung334@gmail.com>
2019-02-06 23:14:20 +0800

    feat: add post store
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from './models/post.model';
+import { PostsStore } from './stores/posts.store';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'ng-rxjs-state-management-demo';
+
+    posts$: Observable<Post[]>;
+
+    constructor(private postsStore: PostsStore) {
+        this.postsStore.init();
+    }
+
+    ngOnInit() {
+        this.posts$ = this.postsStore.getAll$();
+    }
 }
--- src/app/stores/posts.store.ts
+++ src/app/stores/posts.store.ts
@@ -1,4 +1,28 @@
+import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
 import { Post } from '../models/post.model';
+import { PostsService } from '../services/posts.service';
 import { Store } from './store';
 
-export class PostsStore extends Store<Post[]> {}
+@Injectable({
+    providedIn: 'root',
+})
+export class PostsStore extends Store<Post[]> {
+    constructor(private postsService: PostsService) {
+        super();
+    }
+
+    init() {
+        if (this.getAll()) {
+            return;
+        }
+
+        this.postsService
+            .get$()
+            .pipe(
+                tap(posts => console.log('posts', posts)),
+                tap(posts => this.store(posts)),
+            )
+            .subscribe();
+    }
+}
