64acf322e43236b98b61321af9b5a270d719dd8f
Connie Leung <cleung334@gmail.com>
2019-02-07 14:09:59 +0800

    feat: test app-navbar
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,13 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [RouterTestingModule, HttpClientTestingModule],
-            declarations: [AppComponent],
+            declarations: [AppComponent, NavbarComponent],
         }).compileComponents();
     }));
 
@@ -28,5 +29,18 @@ describe('AppComponent', () => {
         fixture.detectChanges();
         const compiled = fixture.debugElement.nativeElement;
         expect(compiled.querySelector('h1').textContent).toContain('Welcome to State management demo in RxJS!');
+    });
+
+    it('should show a navigation bar', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        const navbar = compiled.querySelector('app-navbar');
+
+        expect(navbar).not.toBeUndefined();
+        const menuItems = navbar.querySelectorAll('ul > li') as any[];
+        expect(menuItems.length).toEqual(2);
+        expect(menuItems[0].textContent).toEqual('Posts');
+        expect(menuItems[1].textContent).toEqual('Todos');
     });
 });
